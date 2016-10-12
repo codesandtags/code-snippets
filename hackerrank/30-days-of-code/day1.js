cc28965d75d2e0703ac5509da852c78e2ccce998
Edwin Torres <edwin.torres@prodigious.com>
2016-10-12 10:17:45 -0500

    feat: Adding 30 days of code challenge for Hacker Rank.
    - Was added the solution for the day 1

---
+++ hackerrank/30-days-of-code/day1.js
@@ -0,0 +1,10 @@
+function processData(inputString) {
+    // Your first line of output goes here
+    process.stdout.write("Hello, World.\n");
+
+    // Write the second line of output
+    process.stdout.write(inputString);
+}
+
+// Test
+processData('Welcome to 30 Days of Code!');
