04c49fdac405a7a314bdddc72e4eabb4900d117f
Edwin Torres <codesandtags@gmail.com>
2016-10-12 15:21:49 -0500

    feat: Solution for day2 in 30 days of code using as language JavaScript

---
+++ hackerrank/30-days-of-code/day2.js
@@ -0,0 +1,18 @@
+// https://www.hackerrank.com/challenges/30-data-types
+// Declare second integer, double, and String variables.
+var number, doubleNumber, stringName;
+
+// Read and save an integer, double, and String to your variables.
+number = parseInt(readLine());
+doubleNumber = parseFloat(readLine());
+stringName = readLine();
+
+// Print the sum of both integer variables on a new line.
+console.log(number + i);
+
+// Print the sum of the double variables on a new line.
+console.log( (doubleNumber + d).toFixed(1) );
+
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+console.log(s.concat(stringName));
