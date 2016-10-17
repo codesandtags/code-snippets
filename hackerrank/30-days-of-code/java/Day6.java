766dd80e97dfec4b781f411ffb032d98ad69ca4e
Codes and Tags <codesandtags@gmail.com>
2016-10-17 09:12:35 -0500

    feat: Adding solution for day6 in Java

---
+++ hackerrank/30-days-of-code/java/Day6.java
@@ -0,0 +1,29 @@
+public class Day5 {
+
+    public static void main(String[] args) {
+        // Into this method you can find the solution
+        solution();
+    }
+
+    public static void solution() {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int cases = Integer.parseInt(in.nextLine());
+
+        for (int x = 0; x < cases; x++) {
+            String word = in.nextLine();
+            String odds = "";
+            String evens = "";
+
+            for (int i = 0; i < word.length(); i++) {
+                if (i % 2 == 0) {
+                    evens += word.charAt(i);
+                } else {
+                    odds += word.charAt(i);
+                }
+            }
+
+            System.out.println(evens + " " + odds);
+        }
+    }
+}
